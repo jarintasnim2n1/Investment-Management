3be269c80a72bb12d176fd13b5621ef53e17d10e
Jarin tasnim <163668265+jarintasnim2n1@users.noreply.github.com>
2026-01-22 15:06:12 +0600

    Add files via upload

---
+++ public/page.js
@@ -0,0 +1,11 @@
+"use client";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Image from "next/image";
+import InvestorDashboard from "./components/InvestorDashboard";
+
+export default function Home() {
+  return (
+    <InvestorDashboard/>
+   
+  );
+}
